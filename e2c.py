Fix a bug in KL divergence for the gaussians

e2c.py
@@ -11,22 +11,46 @@ class NormalDistribution(object):
     Cov=A*(sigma).^2*A', where A = (I+v*r^T).
     """
 
-    def __init__(self, mu, sigma, log_sigma, v=None, r=None):
+    def __init__(self, mu, sigma, logsigma, v=None, r=None):
         self.mu = mu
         self.sigma = sigma
-        self.logsigma = log_sigma
+        self.logsigma = logsigma
         self.v = v
         self.r = r
 
+    @property
+    def cov(self):
+        """This should only be called when NormalDistribution represents one sample"""
+        if self.v is not None and self.r is not None:
+            assert self.v.dim() == 1
+            dim = self.v.dim()
+            v = self.v.unsqueeze(1)  # D * 1 vector
+            rt = self.r.unsqueeze(0)  # 1 * D vector
+            A = torch.eye(dim) + v.mm(rt)
+            return A.mm(torch.diag(self.sigma.pow(2)).mm(A.t()))
+        else:
+            return torch.diag(self.sigma.pow(2))
 
-def KLDGaussian(Q, N, eps=1e-9):
-    sum = lambda x: torch.sum(x, dim=1)  # convenience fn for summing over features (columns)
+
+def KLDGaussian(Q, N, eps=1e-8):
+    """KL Divergence between two Gaussians
+        Assuming Q ~ N(mu0, A\sigma_0A') where A = I + vr^{T}
+        and      N ~ N(mu1, \sigma_1)
+    """
+    sum = lambda x: torch.sum(x, dim=1)
     k = float(Q.mu.size()[1])  # dimension of distribution
     mu0, v, r, mu1 = Q.mu, Q.v, Q.r, N.mu
     s02, s12 = (Q.sigma).pow(2) + eps, (N.sigma).pow(2) + eps
     a = sum(s02 * (1. + 2. * v * r) / s12) + sum(v.pow(2) / s12) * sum(r.pow(2) * s02)  # trace term
     b = sum((mu1 - mu0).pow(2) / s12)  # difference-of-means term
-    c = 2. * sum(N.logsigma - Q.logsigma)
+    c = 2. * (sum(N.logsigma - Q.logsigma) - torch.log(1. + sum(v * r)))  # ratio-of-determinants term.
+
+    #
+    # print('trace: %s' % a)
+    # print('mu_diff: %s' % b)
+    # print('k: %s' % k)
+    # print('det: %s' % c)
+
     return 0.5 * (a + b - k + c)
 
 
@@ -63,6 +87,7 @@ class Transition(nn.Module):
 
         d = A.bmm(Q.mu.unsqueeze(2)).add(B.bmm(u)).add(o).squeeze(2)
         sample = A.bmm(h.unsqueeze(2)).add(B.bmm(u)).add(o).squeeze(2)
+
         return sample, NormalDistribution(d, Q.sigma, Q.logsigma, v, r)
 
 
@@ -101,7 +126,7 @@ class Decoder(nn.Module):
 
 
 class E2C(nn.Module):
-    def __init__(self, dim_in, dim_z, dim_u):
+    def __init__(self, dim_in, dim_z, dim_u, lambd=0.5):
         super(E2C, self).__init__()
         self.encoder = Encoder(dim_in, 800)
         self.enc_fc_normal = nn.Linear(800, dim_z * 2)
@@ -109,6 +134,7 @@ class E2C(nn.Module):
         self.decoder = Decoder(dim_z, 800)
         self.dec_fc_bernoulli = nn.Linear(800, dim_in)
         self.trans = Transition(dim_z, dim_u)
+        self.lamdb = lambd
 
     def encode(self, x):
         return F.relu(self.enc_fc_normal(self.encoder(x))).chunk(2, dim=1)
@@ -148,10 +174,10 @@ class E2C(nn.Module):
             # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
             logvar = Qz.logsigma.exp().pow(2).log()
             KLD_element = Qz.mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-            KLD = torch.sum(KLD_element, dim=1).mul_(-0.5)
+            KLD = torch.sum(KLD_element, dim=1).mul(-0.5)
 
             bound_loss = x_reconst_loss.add(x_next_reconst_loss).add(KLD)
-            kl = KLDGaussian(Qz_next_pred, Qz_next).mul(0.5)
+            kl = KLDGaussian(Qz_next_pred, Qz_next).mul(self.lamdb)
             loss = bound_loss.add(kl)
             return loss.mean()
 