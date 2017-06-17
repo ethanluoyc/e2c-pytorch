890ea57a7111d6eed576087409abba9c9368e1d0
ethanluoyc <ethanluoyc@gmail.com>
2017-06-17 17:34:42 +0100

    update train_plane.py

--- train_plane.py
+++ train_plane.py
@@ -1,33 +1,16 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import sys
 
 from pixel2torque.pytorch.e2c import E2C
-from pixel2torque.pytorch.vae import VAE, latent_loss
 import torch
 from torch import optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from pixel2torque.tf_e2c.plane_data2 import PlaneData
 from pixel2torque.pytorch.datasets import PlaneDataset
-
-
-def show_and_save(img, path):
-    npimg = img.numpy()
-    plt.imsave(path, np.transpose(npimg, (1, 2, 0)))
-
-
-def weights_init(m):
-    from torch.nn.init import xavier_uniform
-    if isinstance(m, torch.nn.Linear):
-        xavier_uniform(m.weight.data)
-
-
-def compute_loss(reconst, actual, mean, var):
-    ll = latent_loss(mean, var)
-    reconst_loss = torch.sum(reconst.sub(actual).pow(2), dim=1)
-    loss = reconst_loss.add(ll).mean()
-    return loss
 
 
 def parse_args():
@@ -37,40 +20,60 @@ def parse_args():
     parser.add_argument('--seed', required=False, default=1234, type=int)
     parser.add_argument('--model-dir', required=True)
     parser.add_argument('--test-frequency', default=100)
-    parser.add_argument('--eval-embedding-frequency', default=200)
+    parser.add_argument('--eval-embedding-frequency', default=100)
     parser.add_argument('--visual', action='store_true')
+    parser.add_argument('--epochs', default=1000)
     return parser.parse_args()
+
+
+def weights_init(m):
+    from torch.nn.init import xavier_uniform, orthogonal
+    if isinstance(m, torch.nn.Linear):
+        xavier_uniform(m.weight.data)
+
+
+def make_model_dir(basedir, params):
+    hyperparams = ['lr']
+    return os.path.join(basedir, *['-'.join([p, str(params[p])]) for p in hyperparams])
+
+
+def checkpoint(epoch, model, model_dir):
+    path = os.path.join(model_dir, 'model.pth-{}'.format(epoch))
+    torch.save(model, path)
+    print('checkpoint saved to {}'.format(path))
 
 
 if __name__ == '__main__':
     args = parse_args()
+
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
     img_width = 40
     width = img_width
     height = width
-    model_dir = args.model_dir
+    model_dir = make_model_dir(args.model_dir, {'lr': args.lr})
 
-    planedata = PlaneData('plane0.npz', 'env0.png')
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir, exist_ok=False)
+
+    planedata = PlaneData('plane0.npz', 'env1.png')
     planedata.initialize()
 
     dataset = PlaneDataset(planedata)
-    trainset = dataset
-    testset = dataset
 
     batch_size = args.batch_size
 
     print('batch_size %d' % batch_size)
 
-    loader = DataLoader(trainset, batch_size, shuffle=False,
-                        drop_last=False)
+    loader = DataLoader(dataset, batch_size, shuffle=False,
+                        drop_last=False, num_workers=4)
 
     input_dim = width * height
     latent_dim = 2
     action_dim = 2
 
-    model = E2C(input_dim, latent_dim, action_dim)
+    model = E2C(input_dim, latent_dim, action_dim, config='plane')
 
     print(model)
     weights_init(model)
@@ -84,7 +87,6 @@ if __name__ == '__main__':
 
     if args.visual:
         fig = plt.figure('Embeddings')
-        plt.ion()
         true_ax = plt.subplot(121)
         pred_ax = plt.subplot(122)
 
@@ -102,17 +104,20 @@ if __name__ == '__main__':
         colors = np.array([i for i in range(len(xs))]) / len(xs)
 
         if args.visual:
+            true_ax.clear()
+            pred_ax.clear()
             true_ax.scatter(ps[:, 0], ps[:, 1], c=colors)
             pred_ax.scatter(xs, ys, c=colors)
-            plt.pause(0.01)
 
         return embeds
 
 
-    for epoch in range(1000):
+    step = 0
+    for epoch in range(args.epochs):
         if args.visual:
-            fig.suptitle('Epoch: {}'.format(epoch))
+            fig.suptitle('Step: {}'.format(step))
         model.train()
+
         for i, (x, u, x_next) in enumerate(loader):
             x = Variable(
                 x.resize_(batch_size, input_dim).float(), requires_grad=False)
@@ -130,11 +135,24 @@ if __name__ == '__main__':
 
             aggregate_loss = loss.data[0]
 
-        if epoch % args.eval_embedding_frequency == 0:
-            embeds = evaluate_embedding(model)
+            if step % 1000 == 0:
+                embeds = evaluate_embedding(model)
+                plt.savefig(os.path.join(model_dir, 'vis_step-{}'.format(step)))
 
-        if epoch % args.test_frequency == 0:
-            checkpoint_filename = 'checkpoint.pth'
-            torch.save(model, checkpoint_filename)
-            print('[Epoch %d] checkpoint saved to %s' % (epoch, checkpoint_filename))
-            print('[Epoch %d] loss: %f' % (epoch, aggregate_loss))
+            if step >= 10e5:
+                sys.exit(0)
+
+            if step % 1000 == 0:
+                checkpoint(step, model, model_dir)
+
+            if step % 1000 == 0:
+                print('Step: {}, Loss: {}'.format(step, aggregate_loss))
+
+            step += 1
+
+            # if epoch % args.eval_embedding_frequency == 0:
+            #     embeds = evaluate_embedding(model)
+            #
+            # if epoch % args.test_frequency == 0:
+            #     checkpoint(epoch, model, model_dir)
+            #     print('[Epoch %d] loss: %f' % (epoch, aggregate_loss))
