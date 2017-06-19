adba18046c3c3265ea404943f9690dd66e206470
Yicheng Luo <yicheng.luo16@imperial.ac.uk>
2017-06-19 17:27:03 +0100

    add GymPendulumDataset, require a patched version of gym.

--- datasets.py
+++ datasets.py
@@ -3,6 +3,7 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+import gym
 import torch
 import torchvision
 from mpl_toolkits.mplot3d import Axes3D
@@ -11,6 +12,7 @@ from skimage.transform import resize
 
 from torch.utils.data import Dataset
 from pixel2torque.tf_e2c.plane_data2 import T, num_t
+
 
 
 class PendulumData(Dataset):
@@ -67,3 +69,36 @@ class PlaneDataset(Dataset):
         x_next = np.array(self.planedata.getX(index, t + 1))
         u = np.copy(self.planedata.U[index, t, :])
         return x, u, x_next
+
+
+class GymPendulumDataset(Dataset):
+    """Sample from the OpenAI Gym environment, requires a patched version of gym"""
+    def __init__(self, filename):
+        self._data = np.load(filename)
+
+    def __len__(self):
+        return len(self._data['X0'])
+
+    def __getitem__(self, index):
+        return self._data['X0'][index], self._data['U'][index], self._data['X1'][index]
+
+    @classmethod
+    def sample_trajectories(self, filename, sample_size, step_size=1):
+        _env = gym.make('Pendulum-v0').env
+        X0 = np.zeros((sample_size, 500, 500, 3), dtype=np.uint8)
+        U  = np.zeros((sample_size, 1), dtype=np.float32)
+        X1 = np.zeros((sample_size, 500, 500, 3), dtype=np.uint8)
+        for i in range(sample_size):
+            th = np.random.uniform(0, np.pi*2)
+            thdot = np.random.uniform(-8, 8)
+            state = np.array([th, thdot])
+            initial = state
+            u = np.random.uniform(-2, 2, size=(1,))
+            for i in range(step_size):
+                state = _env.step_from_state(state, u)
+
+            X0[i, :, :, :] = _env.render_state(initial[0])
+            U[i, :] = u
+            X1[i, :, :, :] = _env.render_state(state[0])
+
+        np.savez(filename, X0=X0, U=U, X1=X1)
